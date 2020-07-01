7c9a2e1fc6d7753ee3591c64179da138f7f93ab0
gocreg <jackie121973@gmail.com>
2020-07-01 16:44:43 +0300

    main.js mod

--- Full__course_for_javascript/main.js
+++ Full__course_for_javascript/main.js
@@ -1,2 +1,19 @@
 "use strict";
- 
+let numberJfFilms = prompt('Сколько фильмов Вы посмотрели сегодня?');
+const personalMovieDB = {
+        count: numberJfFilms,
+        movies: {},
+        actors: {},
+        genres: [],
+        privat:false
+};
+
+const   a = prompt('Один из последних фильмов', ''),
+        b = prompt('На сколько его цените', ''),
+        c = prompt('Один из последних фильмов', ''),
+        d = prompt('На сколько его цените', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
